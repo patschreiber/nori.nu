3faa22a38ed85d39f53f0dc4d183f6ddb27f376c
patricks <patrick@nori.nu>
2013-08-26 17:04:54 -0500

    user sign in working

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    user.update_attribute( :remember_token, User.encrypt(remember_token) )
     self.current_user = user
   end
 
@@ -11,11 +11,17 @@ module SessionsHelper
   end
 
   def current_user=(user)
-    @current_user = user_from_remember_token
+    @current_user = user
   end
 
   def current_user
-    @current_user 
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 
 
