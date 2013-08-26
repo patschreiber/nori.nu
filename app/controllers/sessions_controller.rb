1627dc5c75a65e8d2324f9948f73878be05749ce
patricks <patrick@nori.nu>
2013-08-26 16:52:54 -0500

    working with user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by( username: params[:session][:username].downcase )
     if user && user.authenticate( params[:session][:password] )
-      sign_in user
+      sign_in user  # sign_in(user)
       redirect_to user
     else
       flash.now[:error] = 'Invalid username or password'
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130815023426) do
+ActiveRecord::Schema.define(version: 20130826164947) do
+
+  create_table "users", force: true do |t|
+    t.string   "last_name"
+    t.string   "first_name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
