a71cb5b38030d3b86173adc3a593490f57151d72
patricks <patrick@nori.nu>
2013-09-06 17:00:11 -0500

    Stylings and such

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     "nori_logo.svg"
   end
 
+  def current_year
+    "2013"
+  end
+
+  def copyright
+    "© 2013 nori.nu"
+  end
+
+  def admin_social_websites
+    "http://www.twitter.com/patschreiber"
+  end
+
 end
