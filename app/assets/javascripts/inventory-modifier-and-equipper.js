4b66070c968f3271bc06cb4257a7961e237b06be
patricks <patrick@nori.nu>
2013-12-23 17:03:29 -0600

    more work on popover

--- app/assets/javascripts/inventory-modifier-and-equipper.js
+++ app/assets/javascripts/inventory-modifier-and-equipper.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
+  $('.inventory-item').on('mouseenter', function() {
+    $(this).popover({
+      html: true,
+      trigger: 'manual', 
+      delay: {show: 300, hide: 0},
+      title: 'sup',
+      content: 'This button was specified in the initial HTML document'
+    }).popover('show');
+  })
+  .on('mouseleave', function() {
+    $(this).not('.active').popover('hide');
+  })
+  .on('click', function() {
 
-    $('.inventory-item').click(function() {
-      var id = this.id;
-      $(this).popover({
-        html: true
-      }).popover('show');
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    }
+    else {
+      $(this).addClass('active');
+    }
+  });
 
-    });
+
 
   //Hover effect for items in the inventory
   //Equip Items
@@ -15,3 +30,24 @@ $(document).ready(function() {
 
 
 });
+
+
+/*
+    $('.inventory-item').mouseenter(function() {
+      $(this).popover({
+        html: true,
+        trigger: 'manual', 
+        delay: {show: 300, hide: 0},
+        title: 'sup',
+        content: 'This button was specified in the initial HTML document',
+      }).popover('show');
+    })
+    .mouseleave(function() {
+      $(this).popover('hide');
+    });
+
+
+    */
+
+
+// Ajax call http://stackoverflow.com/questions/14736604/twitter-bootstrap-popover-and-ajax
