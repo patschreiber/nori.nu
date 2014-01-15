8f25eb79eb344e5a5e183d4f25f95f3ed7b298a1
patricks <patrick@nori.nu>
2014-01-15 11:11:44 -0600

    reroute work for failed signup attempt

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,7 +12,26 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    super
+
+    build_resource(sign_up_params)
+
+    if resource.save
+      yield resource if block_given?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      redirect_to root_path
+    end
+
+
     @user_stats = UsersStat.new(  
       :user_id => @user.id, 
       :player_level => 1, 
